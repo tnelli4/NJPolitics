f9ac162cc0b42d69780a52d7e39706be140220e3
tnelli4 <tommylcarp@gmail.com>
2025-12-05 18:52:58 -0500

    First graph 90% done!

--- SetupTesting.R
+++ SetupTesting.R
@@ -19,18 +19,28 @@ ggplot(nj_counties) +
   )
 
 # -------------------------------------------------------------------------
+nj_2017_results <- read.csv("nj_gov_2017_csv.txt")
+nj_counties <- st_read("NJ_Counties_3857.shx")
+nj_2017_results_sf <-left_join(nj_counties, nj_2017_results, by = "COUNTY") #left join to keep it as sf
 
-NJ_2017_results <- read.csv("nj_gov_2017_csv.txt")
+nj_2017_results_sf$total_votes <- nj_2017_results_sf$PHILIP.MURPHY..Democratic. + nj_2017_results_sf$KIM.GUADAGNO..Republican.
+nj_2017_results_sf$winner_votes <- pmax(nj_2017_results_sf$PHILIP.MURPHY..Democratic., nj_2017_results_sf$KIM.GUADAGNO..Republican.)
+nj_2017_results_sf$loser_votes <- pmin(nj_2017_results_sf$PHILIP.MURPHY..Democratic., nj_2017_results_sf$KIM.GUADAGNO..Republican.)
+nj_2017_results_sf$margin_positive <- ((nj_2017_results_sf$winner_votes - nj_2017_results_sf$loser_votes)/nj_2017_results_sf$total_votes)*100
+nj_2017_results_sf$margin <- ((nj_2017_results_sf$PHILIP.MURPHY..Democratic. - nj_2017_results_sf$KIM.GUADAGNO..Republican.)/nj_2017_results_sf$total_votes)*100
 
-NJ_2017_results$margin = NJ_2017_results$PHILIP.MURPHY..Democratic./NJ_2017_results$KIM.GUADAGNO..Republican.
-NJ_2017_results <- full_join(NJ_2017_results, nj_counties, by = "COUNTY")
-
-ggplot(NJ_2017_results) +
+ggplot(nj_2017_results_sf) +
   geom_sf(aes(fill = margin)) +
+  scale_fill_gradient2(
+    low = "red",
+    mid = "whitesmoke",
+    high = "blue",
+    midpoint = 0
+  )+
   theme_void()+
   labs(
-    title = "Title of Graph",
-    subtitle = "Subtitle of Graph",
-    fill = "Var Fill Here",
-    caption = "Source of data"
+    title = "2017 NJ Governor Election Margin",
+    subtitle = "Democratic (blue) vs Republican (red)",
+    fill = "Margin win (%)",
+    caption = "Source: NJ DOE"
   )
